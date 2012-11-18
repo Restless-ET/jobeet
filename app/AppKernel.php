cc0125746dba822eb2f8bc84a8cc78eec36110bc
Restless ET <restless86@gmail.com>
2012-11-18 12:39:03 +0000

    Installed and configured Sonata and Knp bundles.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,12 @@ class AppKernel extends Kernel
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new Sonata\AdminBundle\SonataAdminBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            new Sonata\CacheBundle\SonataCacheBundle(),
+            new Sonata\jQueryBundle\SonatajQueryBundle(),
+            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new Ens\JobeetBundle\EnsJobeetBundle(),
         );
 
